0f9262bb740b0f28cdd410f64dbe9e220ff2a8b0
Raz <razibh@gmail.com>
2014-06-01 10:55:01 +1000

    Java8 Predicate Example

---
+++ FunctionalJava8/src/examples/functional/IsPrime.java
@@ -0,0 +1,20 @@
+package examples.functional;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class IsPrime {
+	
+	static boolean isPrime(int number) {
+		IntPredicate isDivisible = divisor -> number % divisor == 0;
+		return number > 1 && IntStream.range(2, number)
+									.noneMatch(isDivisible);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("isPrime(5): " + isPrime(5));
+		System.out.println("isPrime(10): " + isPrime(10));
+		System.out.println("isPrime(11): " + isPrime(11));
+
+	}
+
+}
