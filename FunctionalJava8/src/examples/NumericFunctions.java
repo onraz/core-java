feeb46030a66659d891e3e28c1789326e2310798
Raz <razibh@gmail.com>
2014-06-01 11:16:40 +1000

    refactorings

--- FunctionalJava8/src/examples/NumericFunctions.java
+++ FunctionalJava8/src/examples/NumericFunctions.java
@@ -3,16 +3,11 @@ package examples;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
-public class IsPrime {
+public interface NumericFunctions {
+	
 	static boolean isPrime(int number) {
 		IntPredicate isDivisible = divisor -> number % divisor == 0;
 		return number > 1 && IntStream.range(2, number)
 				.noneMatch(isDivisible);
-	}
-
-	public static void main(String[] args) {
-		System.out.println("isPrime(5): " + isPrime(5));
-		System.out.println("isPrime(10): " + isPrime(10));
-		System.out.println("isPrime(11): " + isPrime(11));
 	}
 }
