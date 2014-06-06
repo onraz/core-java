65dc561697455de774439d6f6eea3fa34d7953cb
Raz <razibh@gmail.com>
2014-06-06 14:58:19 +1000

    test

--- FunctionalJava8/src/examples/FunctionalDeclarativeProgramming.java
+++ FunctionalJava8/src/examples/FunctionalDeclarativeProgramming.java
@@ -2,6 +2,7 @@ package examples;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Coding is communication using the expressiveness of language
@@ -38,5 +39,14 @@ public interface FunctionalDeclarativeProgramming {
 				.filter(name -> name.startsWith("B"))
 				.map(name -> name.toUpperCase())
 				.forEach(System.out::println);
+		
+		Optional<String> value = students.stream()
+				.filter(name -> name.startsWith("K"))
+				.map(name -> name.toUpperCase())
+				.findFirst();
+		
+		value.ifPresent(System.out::println);
+		System.out.println(Optional.empty().isPresent()); 	// false
+		System.out.println(Optional.empty().orElse("raz"));	// raz
 	}
 }
